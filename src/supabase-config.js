622d7776b7ee75f57c9354ea056fe83b56f7a069
Bilal <bilal.swe.ite@gmail.com>
2026-03-31 23:08:06 +0300

    sync files

--- src/supabase-config.js
+++
@@ -1,5 +0,0 @@
-// DropIt — Supabase Configuration
-
-export const SUPABASE_URL = 'https://vgpkkyrwrmitrdipqgjf.supabase.co'
-
-export const SUPABASE_ANON_KEY = 'sb_publishable_opZbUkbwbeTdKwLPND3chw_8QbA1Tsf'
